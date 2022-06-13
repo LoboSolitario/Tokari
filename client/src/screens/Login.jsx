6e85cf6fbfd6d8568756a23a28cbf054be8bc719
dorj222 <dorj222@gmail.com>
2022-06-13 22:38:09 +0200

    create a branch for auth

---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopNavbar from "../components/Nav/TopNavbar";
+import Footer from "../components/Sections/Footer"
+
+export default function Login() {
+  return (
+    <div>
+          <TopNavbar />
+            <p>Hello world!</p>
+          <Footer />
+    </div>
+  )
+}
