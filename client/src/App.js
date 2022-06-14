66d14487aee3ca84c5f7651fe38befb44c51c3ba
dorj222 <dorj222@gmail.com>
2022-06-14 14:35:09 +0200

    react router dom 6 has been implemented; bootstrap 5 has been added; improved and changed the Top Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
 // Screens
 import Landing from "./screens/Landing.jsx";
+import Login from "./screens/Login.jsx";
+
 
 export default function App() {
   return (
     <>
-      <Landing />
+    <Router>
+      <Routes>
+        <Route element={<Landing />} path="/" exact/>
+        <Route element={<Login/>} path="/login"/>
+      </Routes>
+    </Router>
     </>
   );
 }
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -4,10 +4,9 @@ import Footer from "../components/Sections/Footer"
 
 export default function Login() {
   return (
-    <div>
+    <>
           <TopNavbar />
-            <p>Hello world!</p>
-          <Footer />
-    </div>
+          <Footer/>
+    </>
   )
 }
