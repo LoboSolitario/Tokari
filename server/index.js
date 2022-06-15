3265038a7db96f21016648615c2c93325f676352
Shouvik Ghosh <shouvik.ghosh@tum.de>
2022-06-15 12:11:09 +0200

    add custom error handler

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const cors = require('cors');
 const app = express();
 const routeUsers = require('./src/routes/user.js');
 const routeBaskets = require('./src/routes/basket.js');
-
+const {errorHandler} = require('./src/middleware/errorHandler')
+ 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-
 
 app.use('/api/users', routeUsers, (req, res) => res.sendStatus(401));
 app.use('/api/baskets', routeBaskets, (req, res) => res.sendStatus(401));
@@ -17,6 +17,8 @@ app.use('/api/baskets', routeBaskets, (req, res) => res.sendStatus(401));
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
+app.use(errorHandler)
 
 const port = process.env.PORT || 4600;
 app.listen(port);
