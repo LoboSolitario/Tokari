8df153f446771f983771155b708994793fe5ec09
Shouvik Ghosh <shouvik.ghosh@tum.de>
2022-06-22 16:38:27 +0200

    fix errorhandling for user registration

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -7,10 +7,10 @@ const asyncHandler = require('express-async-handler')
 // @desc get list of all users
 // @route GET /api/users
 // @access Private
-const getAllUsers = (req,res) => {
+const getAllUsers = (req, res) => {
     User.find({})
-    .then(users => res.json(users))
-    .catch(err => res.status(500).json({ error: err }));
+        .then(users => res.json(users))
+        .catch(err => res.status(500).json({ error: err }));
 }
 
 // @desc get details about the specific user
@@ -18,8 +18,8 @@ const getAllUsers = (req,res) => {
 // @access Private
 const getUser = asyncHandler(async (req, res) => {
     res.status(200).json(req.user)
-  })
-  
+})
+
 // @desc login a user.
 // @route POST /api/users/login
 // @access public
@@ -27,47 +27,53 @@ const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
     // Check for user email
     const user = await User.findOne({ email })
-  
+
     if (user && (await bcrypt.compare(password, user.password))) {
-      res.json({
-        _id: user.id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id)
-      })
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
     } else {
-      res.status(400)
-      throw new Error('Authentication Failed')
+        res.status(400)
+        throw new Error('Authentication Failed')
     }
-  })
+})
 
 // @desc Register new user
 // @route POST /api/users/register
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const {name, email, password, role} = req.body;
+    const { name, email, password, role } = req.body;
 
-    if (!name||!email||!password||!role){
+    if (!name || !email || !password || !role) {
         res.status(400)
         throw new Error('Please add all fields')
     }
 
-    const userExists = await User.findOne({email})
+    const userExists = await User.findOne({ email })
 
-    if(userExists){
+    if (userExists) {
         res.status(400)
         throw new Error('User already exists')
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt)
+    let newUser;
+    try {
+        newUser = await User.create({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            role: role
+        })
+    } catch (error) {
+        res.status(400);
+        throw new Error(error)
+    }
 
-    const newUser = await User.create({
-        name: name,
-        email: email,
-        password: hashedPassword,
-        role: role       
-    });
-    if(newUser){
+    if (newUser) {
         res.status(201).json({
             _id: newUser.id,
             name: newUser.name,
@@ -75,14 +81,11 @@ const registerUser = asyncHandler(async (req, res) => {
             role: newUser.role,
             token: generateToken(newUser._id)
         })
-    }else{
+    } else {
         res.status(400);
         throw new Error('Invalid User data');
     }
-    // newUser.save()
-    //     .then(user => res.json(user))
-    //     .catch(err => res.json(500, err));
-}) 
+})
 
 // @desc delete user account.
 // @route DELETE /api/users/deleteUser/:id
@@ -108,7 +111,7 @@ const updateUser = (req, res) => {
 // Generate JWT
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
+        expiresIn: '30d',
     })
 }
 
