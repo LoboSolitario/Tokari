739e30006d89fb60e7fbc95f776a73e04f85b996
Xinyao <liuxinyao97@outlook.com>
2022-06-30 22:07:27 +0200

    improve detail page ui

--- client/src/screens/Detail.jsx
+++ client/src/screens/Detail.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import styled from "styled-components";
 import { Routes, Route, useLocation } from 'react-router-dom'
 import TopNavbar from '../components/Nav/TopNavbar';
 import BasketDetail from '../components/Discover/BasketDetail';
@@ -27,8 +28,8 @@ console.log(basket);
                   <div>
                     <div className="flexSpaceNull">
                       <div>
-                        <h3 className="font20 extraBold">{basket.basketName}</h3>
-                        <p className="font11">Managed by {basket.owner}</p>
+                        <h3 className="box font30 extraBold">{basket.basketName}</h3>
+                        <p className="box font11">Managed by {basket.owner}</p>
                       </div>  
                       {basket.subscriptionFee==0?<FreeIcon />: ""}
                     </div>
@@ -41,67 +42,54 @@ console.log(basket);
                     <p className={' tag  radius6 font11 extraBold '+ (basket.volatility=="High"? "redBg" : basket.volatility=="Medium"? "orangeBg":"greenBg")}>Volatility: {basket.volatility}</p>
                   </div>
                 </div>
-                <div className="flexSpaceCenter">
+                <div className="flexSpaceNull">
                   <div className="flexWrapper70">
-                    <div className="semiBold font11 purpleColor borderShow">
-                      <FontAwesomeIcon className="font14 purpleColor" icon={faPlusCircle}/>
+                    <div className="box semiBold font11 purpleColor borderShow radius6">
+                      <FontAwesomeIcon className="font14 purpleColor" style={{ padding: "0 5px" }} icon={faPlusCircle}/>
                       {/* <p className="font15">  </p> */}
                       Trending
-                      <p className="font11 darkColor regular">Watchlisted by over 10K investors</p>
+                      <p className="font11 darkColor regular" style={{ padding: "5px 0" }}>Watchlisted by over 10K investors</p>
                     </div>
                     <p className="borderBottom">Overview & Weights</p>
                     <div>
-                      <p>At a Glance</p>
+                      <p className='box semiBold'>At a Glance</p>
                       <div className='flexSpaceCenter borderBottom'>
                         <div className='flexWrapper20'>                          
-                          <p>CryptoCurrencies</p>
-                          <p>4</p>                        
-                          <p>Last Rebalance</p>
-                          <p>4</p>                       
+                          <p className='font13'>CryptoCurrencies</p>
+                          <p className='font13'>4</p>                        
+                          <p className='font13'>Last Rebalance</p>
+                          <p className='font13'>4</p>                       
                         </div>                     
                         <div className='flexWrapper70'>
-                          <p>Rebalance Frequency</p>
-                          <p>4</p>
-                          <p>Next Rebalance</p>
-                          <p>4</p>
+                          <p className='font13'>Rebalance Frequency</p>
+                          <p className='font13'>4</p>
+                          <p className='font13'>Next Rebalance</p>
+                          <p className='font13'>4</p>
                         </div>
                       </div>                     
                     </div>
-                    <div>
-                      <div className='flexSpaceCenter'>
-                        <p className='flexWrapper70'>CryptoCurrency</p>
-                        <p className='flexWrapper20'>Weightage(%)</p>
+                    <Wrapper>
+                      <p className='box semiBold'>CryptoCurrencies & Weights</p>
+                      <div className='flexSpaceCenter' style={{ padding: "0 0 5px 0" }}>
+                        <p className='flexWrapper70 font13'>CryptoCurrency</p>
+                        <p className='flexWrapper20 font13'>Weightage(%)</p>
                       </div>
                     
                     {(basket.cryptoAlloc) && basket.cryptoAlloc.map(allocation => 
                       <div className='flexSpaceCenter'>
-                        <p className='flexWrapper70'>{allocation.cryptoSymbol}</p>
-                        <p className='flexWrapper20'>{allocation.weight}</p>
+                        <p className='flexWrapper70 font13'>{allocation.cryptoSymbol}</p>
+                        <p className='flexWrapper20 font13'>{allocation.weight}</p>
                       </div>)}
-                    </div>
+                    </Wrapper>
                   </div>
                   <div className="flexWrapper20">
-                    <p>Subscription Fee</p> 
-                    <p>{basket.subscriptionFee} €</p>
+                    <div className='box textCenter'>
+                      <p>Subscription Fee</p> 
+                      <p>{basket.subscriptionFee} €</p>
+                    </div>                   
                     <FullButton title={basket.subscriptionFee==0 ? "Invest Now" : "Subscribe Now"}/>
                   </div>
-                </div>
-
-              {/* <div>
-                <p>{basket.createdAt}</p>
-                {(basket.cryptoAlloc) && basket.cryptoAlloc.map(allocation => 
-                  <div>
-                    <p>{allocation.cryptoSymbol}</p>
-                    <p>{allocation.weight}</p>
-                  </div>)}
-                <p>{basket.details}</p>
-                <p>{basket.overview}</p>
-                <p>{basket.owner}</p>
-                <p>{basket.risk}</p>
-                <p>{basket.subscriptionFee}</p>
-                <p>{basket.updatedAt}</p>
-                <p>{basket.volatility}</p>
-              </div> */}
+                </div>       
             </div>
           </div>
         </div>
@@ -111,4 +99,10 @@ console.log(basket);
     
   );
 }
+const Wrapper = styled.div`
+width: 100%;
+text-align: left;
+padding: 20px 0;
+margin-top: 30px;
+`;
 export default Detail
