68d511c73d914842743ae768b99b87ad38cbff0d
Xinyao <liuxinyao97@outlook.com>
2022-06-30 21:10:07 +0200

    rough detail page

--- client/src/screens/Detail.jsx
+++ client/src/screens/Detail.jsx
@@ -9,6 +9,7 @@ import Filtering from '../components/Discover/Filtering';
 import FreeIcon from "../assets/svg/Services/FreeIcon";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import FullButton from '../components/Buttons/FullButton';
 
 // Screens
 function Detail() {
@@ -40,20 +41,53 @@ console.log(basket);
                     <p className={' tag  radius6 font11 extraBold '+ (basket.volatility=="High"? "redBg" : basket.volatility=="Medium"? "orangeBg":"greenBg")}>Volatility: {basket.volatility}</p>
                   </div>
                 </div>
-                <div className="flex flexSpaceCenter">
-                  <div className="flexLeftWrapper">
-                  <div className="semiBold font11 purpleColor borderShow">
-                    <FontAwesomeIcon className="font14 purpleColor" icon={faPlusCircle}/>
-                    {/* <p className="font15">  </p> */}
-                    Trending
-                    <p className="font11 darkColor regular">Watchlisted by over 10K investors</p>
+                <div className="flexSpaceCenter">
+                  <div className="flexWrapper70">
+                    <div className="semiBold font11 purpleColor borderShow">
+                      <FontAwesomeIcon className="font14 purpleColor" icon={faPlusCircle}/>
+                      {/* <p className="font15">  </p> */}
+                      Trending
+                      <p className="font11 darkColor regular">Watchlisted by over 10K investors</p>
+                    </div>
+                    <p className="borderBottom">Overview & Weights</p>
+                    <div>
+                      <p>At a Glance</p>
+                      <div className='flexSpaceCenter borderBottom'>
+                        <div className='flexWrapper20'>                          
+                          <p>CryptoCurrencies</p>
+                          <p>4</p>                        
+                          <p>Last Rebalance</p>
+                          <p>4</p>                       
+                        </div>                     
+                        <div className='flexWrapper70'>
+                          <p>Rebalance Frequency</p>
+                          <p>4</p>
+                          <p>Next Rebalance</p>
+                          <p>4</p>
+                        </div>
+                      </div>                     
+                    </div>
+                    <div>
+                      <div className='flexSpaceCenter'>
+                        <p className='flexWrapper70'>CryptoCurrency</p>
+                        <p className='flexWrapper20'>Weightage(%)</p>
+                      </div>
+                    
+                    {(basket.cryptoAlloc) && basket.cryptoAlloc.map(allocation => 
+                      <div className='flexSpaceCenter'>
+                        <p className='flexWrapper70'>{allocation.cryptoSymbol}</p>
+                        <p className='flexWrapper20'>{allocation.weight}</p>
+                      </div>)}
+                    </div>
                   </div>
-                  </div>
-                  <div className="flexRightWrapper">
-                    Subscription Fee {basket.subscriptionFee} €
+                  <div className="flexWrapper20">
+                    <p>Subscription Fee</p> 
+                    <p>{basket.subscriptionFee} €</p>
+                    <FullButton title={basket.subscriptionFee==0 ? "Invest Now" : "Subscribe Now"}/>
                   </div>
                 </div>
-              <div>
+
+              {/* <div>
                 <p>{basket.createdAt}</p>
                 {(basket.cryptoAlloc) && basket.cryptoAlloc.map(allocation => 
                   <div>
@@ -67,7 +101,7 @@ console.log(basket);
                 <p>{basket.subscriptionFee}</p>
                 <p>{basket.updatedAt}</p>
                 <p>{basket.volatility}</p>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
