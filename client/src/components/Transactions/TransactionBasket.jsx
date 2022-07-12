de57c65ce0429ee77dde99dd25e4477a06ca992f
gencyusufemre <gencyusufemre@outlook.com>
2022-07-12 12:05:11 +0200

    Bitcoin symbols are changed with currency names in transaction page

--- client/src/components/Transactions/TransactionBasket.jsx
+++ client/src/components/Transactions/TransactionBasket.jsx
@@ -4,6 +4,15 @@ import styled from "styled-components";
 const TransactionBasket = ({
   transaction 
 }) => {
+
+  const cryptoMap = new Map();
+  cryptoMap.set('BTCUSDT', "Bitcoin")
+  cryptoMap.set('ETHUSDT', "Ethereum")
+  cryptoMap.set('LTCUSDT', "Litecoin")
+  cryptoMap.set('TRXUSDT', "Tron")
+  cryptoMap.set('XRPUSDT', "XRP")
+  cryptoMap.set('BNBUSDT', "Binancecoin")
+
   return (
     <Wrapper className="whiteBg radius8 shadow basket">
         <div className="wrapper-header flexSpaceCenter">
@@ -15,17 +24,18 @@ const TransactionBasket = ({
         <div>
           <div className="container" style={{padding: "25px 0"}}>
             <div className='flexSpaceCenter' style={{ padding: "10px 0"}}>                 
-              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Order ID</p>
-              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Cryptocurrency</p>
-              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Order Quantity</p>
-              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Price</p>
-              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Total</p>
+              <p className='font14 extraBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Order ID</p>
+              <p className='font14 extraBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Cryptocurrency</p>
+              <p className='font14 extraBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Order Quantity</p>
+              <p className='font14 extraBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Price</p>
+              <p className='font14 extraBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Total</p>
             </div>
             <p></p>     {/* Arranging color orders */}
+            
             {(transaction.cryptoAlloc) && transaction.cryptoAlloc.map(allocation => 
               <div className='flexSpaceCenter tableStripe' style={{ padding: "5px"}}>
                 <p className='flexWrapper30 font13'>{allocation.orderId}</p>
-                <p className='flexWrapper30 font13'>{allocation.cryptoCurrency}</p>                                                       
+                <p className='flexWrapper30 font13'>{cryptoMap.get(allocation.cryptoCurrency)}</p>                                                       
                 <p className='flexWrapper30 font13'>{allocation.orderQty}</p>                                              
                 <p className='flexWrapper30 font13'>{allocation.price}</p>
                 <p className='flexWrapper30 font13'>{(allocation.price * allocation.orderQty).toFixed(3)}</p>
