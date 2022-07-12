b344e0141f076e85089cafdcb2f6fbb7dc4e7ff9
gencyusufemre <gencyusufemre@outlook.com>
2022-07-12 11:49:09 +0200

    transaction page UI is improved

--- client/src/components/Transactions/TransactionBasket.jsx
+++ client/src/components/Transactions/TransactionBasket.jsx
@@ -7,21 +7,29 @@ const TransactionBasket = ({
   return (
     <Wrapper className="whiteBg radius8 shadow basket">
         <div className="wrapper-header flexSpaceCenter">
-          <h3 className="font15 extraBold">
+          <h3 className="font18 extraBold">
             {transaction.basketName}
           </h3>
-          <p className='flexWrapper30 font13'>{transaction.investmentAmount}</p>
+          <p className='flexWrapper30 font13' style={{color: "#7620FF"}}>Investment Amount: ${transaction.investmentAmount}</p>
         </div>
         <div>
-          <div className="container">
-          {(transaction.cryptoAlloc) && transaction.cryptoAlloc.map(allocation => 
-            <div className='flexSpaceCenter tableStripe'> 
+          <div className="container" style={{padding: "25px 0"}}>
+            <div className='flexSpaceCenter' style={{ padding: "10px 0"}}>                 
+              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Order ID</p>
+              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Cryptocurrency</p>
+              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Order Quantity</p>
+              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Price</p>
+              <p className='font13 semiBold flexWrapper30' style={{ padding: "0 0 5px 0px"}}>Total</p>
+            </div>
+            <p></p>     {/* Arranging color orders */}
+            {(transaction.cryptoAlloc) && transaction.cryptoAlloc.map(allocation => 
+              <div className='flexSpaceCenter tableStripe' style={{ padding: "5px"}}>
                 <p className='flexWrapper30 font13'>{allocation.orderId}</p>
                 <p className='flexWrapper30 font13'>{allocation.cryptoCurrency}</p>                                                       
-                <p className='flexWrapper60 font13'>{allocation.orderQty}</p>                                              
+                <p className='flexWrapper30 font13'>{allocation.orderQty}</p>                                              
                 <p className='flexWrapper30 font13'>{allocation.price}</p>
                 <p className='flexWrapper30 font13'>{(allocation.price * allocation.orderQty).toFixed(3)}</p>
-            </div>)}
+              </div>)}
           </div>
         </div>
     </Wrapper>
