3b8a4bd17a577b3da4b586572d9b79d95629dbf1
gencyusufemre <gencyusufemre@outlook.com>
2022-07-16 14:27:59 +0200

    bug on listing baskets on manager page is solved

--- client/src/components/Users/ManagerProfile.jsx
+++ client/src/components/Users/ManagerProfile.jsx
@@ -21,7 +21,10 @@ function ManagerProfile() {
 
         const response = await axios.get(`${baseUrl}/api/users/manager/${managerId}`);   // , { headers: { Authorization: "Bearer: " + token } }
         if (response.status) {
-          console.log(response.data);
+          // Setting baskets id fields to view Baskets
+          response.data.createdBaskets.map((basket) => (
+            basket.id = basket._id
+            ))
           setManager(response.data)
         } else {
           response.json().then((data)=>{
@@ -44,6 +47,7 @@ function ManagerProfile() {
   
       configOptions("GET", headers, options);
       const response = await fetch(`${baseUrl}/api/baskets/basket/${id}`, options);
+      console.log(response);
       if(response.ok){
         response.json().then((data) => {
           navigate(`/basket/${id}`, {state:data});
@@ -56,19 +60,19 @@ function ManagerProfile() {
 
   return (
     <div key={manager._id} className="container70 whiteBg shadow discoverPage flex flexColumn" >
-      <div className='flexWrapper60' style={{padding: '20px 20px 0 20px'}}>
+      <div className='flexWrapper60' style={{padding: '50px 20px 0 20px'}}>
         <h3 className="box font30 extraBold">Manager Profile</h3>
       </div>
       <div className="container"
         style={{
           display: "in-block",
-          marginTop: "50px",
+          marginTop: "20px",
           marginBottom: "0px"
         }}>
         <div className="lightBg metricsBox borderRad ">
           <div className="flex">
             <div className="semiBold font25  flexColBig">
-              Manager Profile
+              {manager.name}
             </div>
             <div className="greyColor flexCol">
               Email Address
@@ -83,16 +87,16 @@ function ManagerProfile() {
           </div>
           <div className="flexRowGap ">
             <div className="greyColor flexColBig">
-              Invest in baskets to see overview here
+              
             </div>
             <div className=" flexCol">
-              totalInvestmentAmount
+              {manager.email}
             </div>
             <div className=" flexCol">
-              subscriptionCount
+              x
             </div>
             <div className=" flexCol">
-              currentBinanceBalance
+              y
             </div>
 
           </div>
@@ -111,42 +115,7 @@ function ManagerProfile() {
       ) : (
         <p>No matching results.</p>
       )}
-      </div>
-
-      
-      <div className="">
-        <div className="flexWrapper70">
-
-          <div className='borderBottom'>
-            <Wrapper style={{ padding: "0 0 20px 0px"}}>
-              <p className='box semiBold'>Detail</p>
-              <p className='font13'>basket.details</p>
-            </Wrapper>
-          </div>
-
-          <div className='borderBottom'>
-            <Wrapper style={{ padding: "0 0 20px 0px"}}>
-            <p className='box semiBold'>At a Glance</p>
-            <div className='flexSpaceCenter' >
-              <div className='flexWrapper60'>                          
-                <p className='font13 semiBold' style={{ padding: "0 0 5px 0px"}}>Cryptocurrencies</p>
-                <p className='font13' style={{ padding: "0 0 7px 0px"}}>basket.cryptoNumber</p>                        
-                <p className='font13 semiBold' style={{ padding: "0 0 5px 0px"}}>Last rebalance</p>
-                <p className='font13'>Jun.20, 2022</p>                       
-              </div>                     
-              <div className='flexWrapper30'>
-                <p className='font13 semiBold' style={{ padding: "0 0 5px 0px"}}>Rebalance frequency</p>
-                <p className='font13' style={{ padding: "0 0 7px 0px"}}>three month</p>
-                <p className='font13 semiBold' style={{ padding: "0 0 5px 0px"}}>Next rebalance</p>
-                <p className='font13'>Sep.20, 2022</p>
-              </div>
-            </div>
-            </Wrapper>                     
-          </div>
-        
-          
-        </div>
-      </div>       
+      </div>    
     </div>
     
   );
