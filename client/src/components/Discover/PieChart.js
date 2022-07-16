fc0b4af27a4cc4a39e67c597e2141d386b408222
Xinyao <liuxinyao97@outlook.com>
2022-07-16 12:56:10 +0200

    add pie chart in basket detail page

---
+++ client/src/components/Discover/PieChart.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from "react";
+import { useLocation } from 'react-router-dom'
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+function PieChartComponent() {
+    const COLORS = ["#8884d8", "#82ca9d", "#FFBB28", "#FF8042", "#AF19FF"];
+    const location = useLocation();
+    const basket = location.state;
+    const [pieData, setPieData] = useState([]);
+
+    useEffect(() => {
+        let temp = [];
+        basket.cryptoAlloc.map(allocation => {
+            let obj = {
+            "name": allocation.cryptoSymbol,
+            "value": allocation.weight
+            }
+            temp.push(obj);
+        })
+        setPieData(temp);
+    },[basket.cryptoAlloc])
+    
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active) {
+            return (
+            <div
+                className="custom-tooltip"
+                style={{
+                backgroundColor: "#ffff",
+                padding: "5px",
+                border: "1px solid #cccc"
+                }}
+            >
+                <label className="font11">{`${payload[0].name} : ${payload[0].value}%`}</label>
+            </div>
+            );
+        }
+        return null;
+    };
+
+    return (
+        <div style={{ width: '100%', height: 210 }}>
+            <ResponsiveContainer>
+                <PieChart>
+                    <Pie
+                        data={pieData}
+                        color="#000000"
+                        dataKey="value"
+                        nameKey="name"
+                        cx="50%"
+                        cy="50%"
+                        outerRadius={80}
+                        fill="#8884d8"
+                    >
+                        {pieData.map((entry, index) => (
+                            <Cell
+                            key={`cell-${index}`}
+                            fill={COLORS[index % COLORS.length]}
+                            />
+                        ))}
+                    </Pie>
+                    <Tooltip content={<CustomTooltip />} />
+                    <Legend wrapperStyle={{fontSize: "12px"}} iconSize={7}/>
+                </PieChart>
+        </ResponsiveContainer>
+        </div>
+    );
+}
+export default PieChartComponent;
