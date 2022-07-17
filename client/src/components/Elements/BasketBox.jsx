ab53857e97d6c8ec7bbe1b2f7b00f397b785890a
dorj222 <dorj222@gmail.com>
2022-07-17 10:17:16 +0200

    update the landing page and add git request to render baskets

--- client/src/components/Elements/BasketBox.jsx
+++ client/src/components/Elements/BasketBox.jsx
@@ -6,12 +6,15 @@ const BasketBox = ({
   risk, 
   basketName, 
   overview, 
-  owner, 
+  author, 
   volatility,
-  subscriptionFee}) => {
+  handleDetailBox,
+  id,
+  subscriptionFee,
+}) => {
     
   return (
-      <div className="whiteBg radius8 shadow h-10 flexBasket">
+      <div className="whiteBg radius8 shadow h-10 flexBasket" style={{margin: "10px"}}>
         <div className="flexHead">
           <div className="flexSpaceNull">
             <h3 className="font20 extraBold">{basketName}</h3>
@@ -24,7 +27,7 @@ const BasketBox = ({
         
         <div className="flexRisk">
           <div className="flexSpaceNull">
-            <p className="font13 extraBold">{owner}</p>
+            <p className="font13 extraBold">{author}</p>
           </div>
           
           <div className="flexSpaceNull" style={{padding: "12px 0 0"}}>
@@ -33,9 +36,9 @@ const BasketBox = ({
           </div>
         </div>
         <div className="flexButton">
-          <div className="row flexHorizontalCenter">
-              <div style={{ width: "150px" }}>
-                <ViewButton title="View Basket" action={() => alert("clicked")} />
+          <div className="flexHorizontalCenter">
+              <div style={{ width: "120px" }}>
+                <ViewButton title="View Basket" action={() => handleDetailBox(id)}/>
               </div>
           </div>
         </div>
