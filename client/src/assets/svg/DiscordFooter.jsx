df4d4b0b4edc65a11f571ba12968a71dba2bb4ac
dorj222 <dorj222@gmail.com>
2022-07-19 23:06:25 +0200

    update Footer and Landing page

---
+++ client/src/assets/svg/DiscordFooter.jsx
@@ -0,0 +1,11 @@
+
+import * as React from "react";
+import {default as logo} from './brands/discord-brands.svg'
+
+function SvgComponent(props) {
+  return (
+    <img src={logo} alt="" style={{height: "25px"}} className="filter-logo"/>
+  );
+}
+
+export default SvgComponent;
